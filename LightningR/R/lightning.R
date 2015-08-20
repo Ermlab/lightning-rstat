61556879b0c7628e166c32f4714d235476ae687a
Kamil Kolodziejski <kolodziejski.kamil@hotmail.com>
2015-08-20 14:07:54 +0200

    fixed display error in pdf

--- LightningR/R/lightning.R
+++ LightningR/R/lightning.R
@@ -18,7 +18,9 @@
 #' @field autoopen Checks if the server is automatically opening the visualizations.
 #' #' @section Methods:
 #' \describe{
-#'   For full documentation of each method go to https://github.com/Ermlab/lightining-rstat/
+#'   \item{Documentation}{For full documentation of each method go to https://github.com/Ermlab/lightining-rstat/}
+#'   \item{\code{new(serveraddress)}}{This method is used to create object of this class with \code{serveraddress} as address of the server object is connecting to.}
+#'
 #'   \item{\code{sethost(serveraddress)}}{This method changes server that you are contacting with to \code{serveraddress}.}
 #'   \item{\code{createsession(sessionname = "")}}{This method creates new session on the server with optionally given name in \code{sessionname}.}
 #'   \item{\code{usesession(sessionid)}}{This method changes currently used session on the server to the one with id given in \code{sessionid} parameter.}
